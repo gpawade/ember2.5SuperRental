45a183abf5313dfc6b8720d65a420ffdd240a53a
gpawade <gap.subscribe@gmail.com>
2017-02-17 22:59:02 +0530

    ember js 2 sample

---
+++ app/components/rental-listing.js
@@ -0,0 +1,13 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+
+    isWide: false,
+
+    actions : {
+        toggleImageSize() {
+            this.toggleProperty('isWide');
+        }
+    }
+    
+});
